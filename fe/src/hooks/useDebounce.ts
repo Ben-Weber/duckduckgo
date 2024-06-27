4b3b059420854a781ef58703fd5e173f3128d195
Ben-Weber <benfweber@gmail.com>
2024-06-28 02:45:21 +0300

    style scroll bar & add JSDoc for useDebounce

--- fe/src/hooks/useDebounce.ts
+++ fe/src/hooks/useDebounce.ts
@@ -1,5 +1,12 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * Custom hook that debounces a value.
+ * @param value - The value to debounce.
+ * @param delay - The delay in milliseconds.
+ * @returns The debounced value.
+ */
+
 const useDebounce = <T,>(value: T, delay: number): T => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
