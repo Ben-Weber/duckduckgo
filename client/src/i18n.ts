b4941cbbe227e9cbb96a13696c855e50eefa6203
Ben-Weber <benfweber@gmail.com>
2024-06-30 23:17:39 +0300

    add hebrew translation

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,13 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import translation from './locale/translation.json';
+import en from '../src/locale/en/translation.json';
+import he from '../src/locale/he/translation.json';
 
 i18n
   .use(initReactI18next)
   .init({
     resources: {
       en: {
-        translation: translation,
+        translation: en,
+      },
+      he: {
+        translation: he,
       },
     },
     lng: 'en',
@@ -18,3 +22,4 @@ i18n
   });
 
 export default i18n;
+
